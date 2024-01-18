48f204120af5d7571afc4a5dca88c15441ea4b3f
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-18 19:25:12 +0530

    added spaces

--- #Operators.py
+++ #Operators.py
@@ -7,18 +7,22 @@ num2 = 20;
 #01 Addition
 addition = num1 + num2;
 print(addition);
+print();
 
 #02 Subtraction
 subtraction = num2 - num1;
 print(subtraction);
+print();
 
 #03 Multiplication
 multiplication = num1 * num2;
 print(multiplication);
+print();
 
 #04 Division
 division = num2 / num1;
 print(division);
+print();
 
 
 #Relational Operators (<,>,==,!=,<=,>=)
@@ -28,26 +32,32 @@ num2 = 9;
 #01 Less than
 lessThan = num1 < num2;
 print(lessThan)
+print();
 
 #02 Greater than
 greaterThan = num1 > num2;
 print(greaterThan)
+print();
 
 #03 Equal to
 equalTo = num1 == num2;
 print(equalTo);
+print();
 
 #04 Not Euqual to
 notEqualTo = num1 != num2;
 print(notEqualTo);
+print();
 
 #05 Less than or equal to
 lessThanEqualTo = num1 <= num2;
 print(lessThanEqualTo)
+print();
 
 #06 Greater than or equal to
 greaterThanEqualTo = num1 >= num2;
 print(greaterThanEqualTo)
+print();
 
 
 # Logical Opeators (and,or,not)
@@ -59,16 +69,19 @@ andOp = bool1 and bool2;
 print(andOp);
 andOp = bool1 & bool2;
 print(andOp);
+print();
 
 #02 Logical or
 orOp = bool1 or bool2;
 print(orOp);
 orOp = bool1 | bool2;
 print(orOp);
+print();
 
 #03 Logical not
 notOp = not bool1;
 print(notOp);
+print();
 
 
 # Bitwise Operator (&(and),|(or),~(not),^(xor),>>(right shift),<<(left shift))
@@ -78,20 +91,24 @@ num2 = 2;  #0010
 #01 Bitwise and
 bitAnd = num1 & num2; #both 1 then 1 otherwise zero
 print(bitAnd);
+print();
 
 #02 Bitwise or
 bitOr = num1 | num2; #both zero then zero otherwise one
 print(bitOr);
+print();
 
 #03 Bitwise xor
 bitXor = num1 ^ num2;  # both same then zero otherwise one
 print(bitXor);
+print();
 
 #04 Left Shift
 leftShift = num1<<3;  # multiply by 2^3
 print(leftShift);
 leftShift = num2<<2;  # multiply by 2^2
 print(leftShift);
+print();
 
 #05 Right Shift
 num1 = 24;
