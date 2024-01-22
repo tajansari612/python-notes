40d434381810ca8a724178d69bbd5769db58b3e9
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-22 14:44:04 +0530

    new file added 05_ControlStatements

---
+++ 05_ControlStatements.py
@@ -0,0 +1,51 @@
+#Conditionals
+
+#01 if-else statements
+num = 78;
+if(num%2==0):
+    print("It is a even number");
+else:
+    print("It is odd number")
+
+#02 elif
+num = 87;
+if(num > 100):
+    print("Greater than 100");
+elif(num > 50):
+    print("Greater than 50");
+else:
+    print("less than 50");
+
+
+#03 match case
+num1 = int(input("Enter first number :"));
+num2 = int(input("Enter second number :"));
+Operator = str(input("Enter the operator :"));
+match Operator:
+    case '+':
+        print(num1+num2);
+    case '-':
+        print(num1-num2);
+    case '*':
+        print(num1*num2);
+    case '/':
+        print(num1/num2);
+    case _ :
+        print("Enter a valid operator");
+
+
+#loops
+#01 for loop
+for i in range(1,10,2):    #range(start,end,step)
+    print(i);
+
+l1 = [1,5,7,3,4,9];
+for i in l1:
+    print(i);
+print()
+
+#02 while loop
+i = 0;
+while(i<10):
+    print(i);
+    i+=1;
