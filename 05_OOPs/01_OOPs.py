ef09253e164a2ab9e4c42a2afd106f04451158c4
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-01-25 11:10:17 +0530

    new file oops created

---
+++ 05_OOPs/01_OOPs.py
@@ -0,0 +1,29 @@
+# In Python, object-oriented Programming (OOPs) is a programming paradigm that
+# uses objects and classes in programming. It aims to implement real-world
+# entities like inheritance, polymorphisms, encapsulation, etc. in the 
+# programming. The main concept of OOPs is to bind the data and the functions 
+# that work on that together as a single unit so that no other part of the code 
+# can access this data.
+
+# Why to use OOPs in python
+#01 Helps to mimic real world entities and their properties
+#02 Code reausablility
+#03 Organization and maintainability of code
+
+
+#OOPs Concepts in Python
+#01 Class
+#02 Objects
+#03 Polymorphism
+#04 Encapsulation
+#05 Inheritance
+#06 Data Abstraction
+
+
+#01 Class
+# A class is a collection of objects. A class contains the blueprints or the 
+# prototype from which the objects are being created. It is a logical entity 
+# that contains some attributes and methods. 
+
+class student :
+    
