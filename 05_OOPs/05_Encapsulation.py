3f4e198f145873705344a6b9632e5ba7474b9f6c
tajansari612 <154970584+tajansari612@users.noreply.github.com>
2024-02-02 23:05:00 +0530

    new file created

---
+++ 05_OOPs/05_Encapsulation.py
@@ -0,0 +1,3 @@
+# Encapsulation in Python is the process of bundling data members and methods 
+# inside a single class. This bundling of data and methods helps in data hiding.
+# Encapsulation also ensures that objects of a class can function independently.
---
+++ 05_OOPs/06_DataAbstraction.py
@@ -0,0 +1,4 @@
+# Data abstraction in Python is a programming concept that hides complex 
+# implementation details while exposing only essential information and 
+# functionalities to users. By doing so, it helps to simplify code and 
+# make it more maintainable.
